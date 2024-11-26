18fe63e7ebf2217edf6b815bc34034a0810f892f
tpdms0159 <tpdms0159@naver.com>
2024-11-26 14:10:11 +0900

    add : docker 이미지 업로드 yaml 파일 생성

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM --platform=linux/amd64 python:3.8-slim-buster as build
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+    postgresql-client \
+    && rm -rf /var/lib/apt/lists/*
+WORKDIR /usr/src/app
+COPY requirements.txt ./
+RUN pip3 install -r requirements.txt 
+COPY . .
+
+EXPOSE 80
+CMD ["python", "manage.py", "runserver", "0.0.0.0:80"]
--- apiserver/settings.py
+++ apiserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-egg8t7=@*0dv8$td4^$e^o!1fzat=pq&cn0j#-$%dci5nq-6zu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apiserver',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
